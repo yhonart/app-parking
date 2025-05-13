1f556733c0f575a2f203fc5631239fd9d5861761
yhONdev <yusuf7789@gmail.com>
2025-05-13 13:52:45 +0700

    change halaman edit kendaraan

---
+++ resources/views/editKendaraan.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+@section('content')
+<?php
+    $no = '1';
+?>
+<div class="content-header">
+    <div class="container">
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0"></h1>
+        </div><!-- /.col -->        
+    </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<div class="content">
+    <div class="container">        
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-primary">                    
+                    <div class="card-body table-responsive">                        
+                        <form id="formEditKendaraan" class="form-horizontal" autocomplete="off">
+                            <div class="row form-group">
+                                <label for="" class="col-md-4 col-form-label">Kode <span class=" text-danger">*</span></label>
+                                <div class="col-md-8">
+                                    <input type="text" name="editKodeKendaraan" id="editKodeKendaraan" class="form-control form-control-sm" value="{{$editKendaraan->barcode}}" disabled>
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <label for="" class="col-md-4 col-form-label">No.QR <span class=" text-danger">*</span></label>
+                                <div class="col-md-8">
+                                    <input type="text" name="noQR" id="noQR" class="form-control form-control-sm" value="{{$editKendaraan->barcode}}" disabled>
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <label for="" class="col-md-4 col-form-label">Pemilik <span class=" text-danger">*</span></label>
+                                <div class="col-md-8">
+                                    <input type="text" name="pemilik" id="pemilik" class="form-control form-control-sm" value="{{$editKendaraan->namaPemilik}}">
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <label for="" class="col-md-4 col-form-label">No.POL <span class=" text-danger">*</span></label>
+                                <div class="col-md-8">
+                                    <input type="text" name="nopol" id="nopol" class="form-control form-control-sm" value="{{$editKendaraan->nopol}}">
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <label for="" class="col-md-4 col-form-label">Manufaktur <span class=" text-danger">*</span></label>
+                                <div class="col-md-8">
+                                    <input type="text" name="manufaktur" id="manufaktur" class="form-control form-control-sm" value="{{$editKendaraan->manufaktur}}">
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <label for="" class="col-md-4 col-form-label">Type Kendaraan <span class=" text-danger">*</span></label>
+                                <div class="col-md-8">
+                                    <input type="text" name="typeKendaraan" id="typeKendaraan" class="form-control form-control-sm" value="{{$editKendaraan->type}}">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
